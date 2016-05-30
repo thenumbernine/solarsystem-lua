df488496cc7aa11fb6b6d1322835528f2f09974d
Christopher Moore <christopher.e.moore@gmail.com>
2016-05-31 00:45:08 +0300

    moar

--- graph.lua
+++ graph.lua
@@ -17,28 +17,52 @@ local function refreshGraphs(startDate, endDate)
 		planets[i] = Planets.fromEphemeris(currentDate + dayOffset, 406, 'eph/406')
 	end
 
+	--[=[ graph angles between planets
 	-- for earth, considering all other planets, find angles between all other planets
 	local names = Planets.indexes:keys()
 	names:removeObject('earth')
+	names:removeObject('moon')
 
 	local graphs = table()
-	for i=1,#names-1 do
-		for j=i+1,#names do
-			local na = names[i]
-			local nb = names[j]
-			graphs[na..' -> earth -> '..nb] = {
-				enabled = true,
-				julianDates,
-				table.map(planets, function(planets)
-					local va = planets[Planets.indexes[na]].pos - planets[Planets.indexes.earth].pos
-					local vb = planets[Planets.indexes[nb]].pos - planets[Planets.indexes.earth].pos
-					va = va:normalize()
-					vb = vb:normalize()
-					return math.deg(math.acos(math.clamp(va:dot(vb), -1, 1)))
-				end),
-			}
-		end
+	for i=1,#names do
+		local na = names[i]
+		local nb = 'moon' 
+		graphs[na..' -> earth -> '..nb] = {
+			enabled = true,
+			julianDates,
+			table.map(planets, function(planets)
+				local va = planets[Planets.indexes[na]].pos - planets[Planets.indexes.earth].pos
+				local vb = planets[Planets.indexes[nb]].pos - planets[Planets.indexes.earth].pos
+				va = va:normalize()
+				vb = vb:normalize()
+				return math.deg(math.acos(math.clamp(va:dot(vb), -1, 1)))
+			end),
+		}
 	end
+	for _,k in ipairs{0,90,120,150,180} do
+		graphs[tostring(k)..' degrees'] = {
+			enabled = true,
+			color = {1,1,1},
+			{startDate, endDate},
+			{k,k},
+		}
+	end
+	--]=]
+	-- [=[ graph planet distances
+	local names = Planets.indexes:keys()
+	names:removeObject('earth')
+	local graphs = table()
+	for i,name in ipairs(names) do
+		graphs[name..' <-> earth'] = {
+			enabled = true,
+			julianDates,
+			table.map(planets, function(planets)
+				return (planets[Planets.indexes[name]].pos - planets[Planets.indexes.earth].pos):length()
+			end),
+		}
+	end
+	--]=]
+
 	return graphs
 end
 
@@ -60,7 +84,11 @@ function App:update()
 	then
 		print('refreshing to ',self.viewbbox.min[1], self.viewbbox.max[1])
 		self.dontResetView = true
-		self:setGraphInfo(refreshGraphs(self.viewbbox.min[1], self.viewbbox.max[1]))
+		local newGraphs = refreshGraphs(self.viewbbox.min[1], self.viewbbox.max[1])
+		for name,graph in pairs(self.graphs) do 
+			graph[1] = newGraphs[name][1]
+			graph[2] = newGraphs[name][2]
+		end
 		self.dontResetView = false
 		self.viewbboxLast.min[1] = self.viewbbox.min[1]
 		self.viewbboxLast.max[1] = self.viewbbox.max[1]
