a15212d21f3b56f74cf9e323e519381d383e6a8f
Christopher Moore <christopher.e.moore@gmail.com>
2018-07-28 00:12:29 +0200

    more options

--- graph.lua
+++ graph.lua
@@ -45,57 +45,98 @@ function App:refreshGraphs(startDate, endDate)
 	end
 
 	-- [=[ graph angles between planets
-	local graphs = table()
-	for i,ni in ipairs(planetNames) do
-		for j,nj in ipairs(planetNames) do
-			for k,nk in ipairs(planetNames) do
-				local name = table{ni,nj,nk}:concat' -> '
-				if self.graphsEnabledForName[name] then
-					graphs[name] = {
-						enabled = true,
-						julianDates,
-						table.map(planets, function(planets,i)
-							local va = planets[Planets.indexes[ni]].pos - planets[Planets.indexes[nj]].pos
-							local vb = planets[Planets.indexes[nk]].pos - planets[Planets.indexes[nj]].pos
-							va = va:normalize()
-							vb = vb:normalize()
-							local theta = math.deg(math.acos(math.clamp(va:dot(vb), -1, 1)))
-							return theta
-						end),
-					}
+	local graphs = self.graphs or table()
+	for j=1,#planetNames do
+		local nj = planetNames[j]
+		for i=1,#planetNames-1 do
+			if i ~= j then
+				local ni = planetNames[i]
+				for k=i+1,#planetNames do
+					if k ~= j then
+						local nk = planetNames[k]
+						local name = table{ni,nj,nk}:concat' -> '
+						local name2 = table{nk,nj,ni}:concat' -> '
+						if self.angleGraphsEnabledForName[name] 
+						or self.angleGraphsEnabledForName[name2]
+						then
+							local srcGraph = graphs[name]
+							local srcEnabled = true
+							if srcGraph then srcEnabled = srcGraph.enabled end
+							graphs[name] = {
+								enabled = srcEnabled,
+								color = srcGraph and srcGraph.color or nil,
+								julianDates,
+								table.map(planets, function(planets,i)
+									local va = planets[Planets.indexes[ni]].pos - planets[Planets.indexes[nj]].pos
+									local vb = planets[Planets.indexes[nk]].pos - planets[Planets.indexes[nj]].pos
+									va = va:normalize()
+									vb = vb:normalize()
+									local theta = math.deg(math.acos(math.clamp(va:dot(vb), -1, 1)))
+									return theta
+								end),
+							}
+						else
+							if graphs[name] then
+								graphs[name] = nil
+							end
+						end
+					end
 				end
 			end
 		end
 	end
+	
+	for i=1,#planetNames-1 do
+		if i ~= j then
+			local ni = planetNames[i]
+			for k=i+1,#planetNames do
+				if k ~= j then
+					local nk = planetNames[k]
+					local name = table{ni,nk}:concat' <-> '
+					local name2 = table{nk,ni}:concat' <-> '
+					if self.distanceGraphsEnabledForName[name] 
+					or self.distanceGraphsEnabledForName[name2]
+					then
+						local srcGraph = graphs[name]
+						local srcEnabled = true
+						if srcGraph then srcEnabled = srcGraph.enabled end
+						graphs[name] = {
+							enabled = srcEnabled,
+							color = srcGraph and srcGraph.color or nil,
+							julianDates,
+							table.map(planets, function(planets)
+								return (planets[Planets.indexes[ni]].pos - planets[Planets.indexes[nk]].pos):length()
+							end),
+						}
+					else
+						if graphs[name] then
+							graphs[name] = nil
+						end
+					end
+				end
+			end
+		end
+	end
+	
 	for _,k in ipairs{0,90,120,150,180} do
-		graphs[tostring(k)..' degrees'] = {
-			enabled = true,
-			color = {1,1,1},
+		local name = tostring(k)..' degrees'
+		local srcGraph = graphs[name]
+		local srcEnabled = true
+		if srcGraph then srcEnabled = srcGraph.enabled end
+		graphs[name] = {
+			enabled = srcEnabled,
+			color = srcGraph and srcGraph.color or {1,1,1},
 			{startDate, endDate},
 			{k,k},
 		}
 	end
-	--]=]
-	--[=[ graph planet distances
-	local names = Planets.indexes:keys()
-	names:removeObject('earth')
-	local graphs = table()
-	for i,name in ipairs(names) do
-		graphs[name..' <-> earth'] = {
-			enabled = true,
-			julianDates,
-			table.map(planets, function(planets)
-				return (planets[Planets.indexes[name]].pos - planets[Planets.indexes.earth].pos):length()
-			end),
-		}
-	end
-	--]=]
 
 	return graphs
 end
 
 function App:init()
-	self.graphsEnabledForName = table()
+	self.angleGraphsEnabledForName = table()
+	self.distanceGraphsEnabledForName = table()
 	--local graphs = self:refreshGraphs(-100, 100)	-- before refresh ...
 	App.super.init(self, {})
 	self.viewsize = vec2(100, 100)
@@ -138,96 +179,171 @@ end
 
 function App:getCoordText()
 	local j = julian.toCalendar(mouseTime + currentDate)
-	return 'mouse angle: '..tolua(mouseAngle)..'\n'
-		..'mouse time (jul): '..tolua(mouseTime)..'\n'
+	return 'graph y: '..tolua(mouseAngle)..'\n'
+		..'mouse time (+jul): '..tolua(mouseTime)..'\n'
 		..'mouse time (greg): '..gregstr(j)
 end
 
 local bool = ffi.new'bool[1]'
 function App:updateGUI()
-	ig.igPushIDStr'planetCheckboxes'
-	for j,nj in ipairs(planetNames) do
-		ig.igPushIDStr(''..j)
-		ig.igText(nj..':')
-		ig.igSameLine()
-		if ig.igCollapsingHeader'' then
-			local all = true	
-			for i=1,#planetNames-1 do
-				if i ~= j then
-					for k=i+1,#planetNames do
-						if k ~= j 
-						and i ~= k 
-						then
-							local ni = planetNames[i]
-							local nk = planetNames[k]
-							local name = table{ni,nj,nk}:concat' -> '
-							local name2 = table{nk,nj,ni}:concat' -> '
-							if not self.graphsEnabledForName[name] 
-							and not self.graphsEnabledForName[name2] 
-							then
-								all = false
-								break
-							end
-						end
-						if not all then break end
-					end
-					if not all then break end
-				end
-			end
-			bool[0] = all
-			if ig.igCheckbox('all', bool) then
-				all = bool[0]
+	if ig.igCollapsingHeader'conjunctions:' then
+		ig.igPushIDStr'angleGraphs'
+		for j,nj in ipairs(planetNames) do
+			ig.igPushIDStr(''..j)
+			ig.igText(nj..':')
+			ig.igSameLine()
+			if ig.igCollapsingHeader'' then
+				local all = true	
 				for i=1,#planetNames-1 do
 					if i ~= j then
 						for k=i+1,#planetNames do
 							if k ~= j 
-							and i ~= k
+							and i ~= k 
 							then
 								local ni = planetNames[i]
 								local nk = planetNames[k]
 								local name = table{ni,nj,nk}:concat' -> '
 								local name2 = table{nk,nj,ni}:concat' -> '
-								self.graphsEnabledForName[name] = all
-								self.graphsEnabledForName[name2] = nil
-								self.changedCheckbox = true
+								if not self.angleGraphsEnabledForName[name] 
+								and not self.angleGraphsEnabledForName[name2] 
+								then
+									all = false
+									break
+								end
+							end
+							if not all then break end
+						end
+						if not all then break end
+					end
+				end
+				bool[0] = all
+				if ig.igCheckbox('all', bool) then
+					all = bool[0]
+					for i=1,#planetNames-1 do
+						if i ~= j then
+							for k=i+1,#planetNames do
+								if k ~= j 
+								and i ~= k
+								then
+									local ni = planetNames[i]
+									local nk = planetNames[k]
+									local name = table{ni,nj,nk}:concat' -> '
+									local name2 = table{nk,nj,ni}:concat' -> '
+									self.angleGraphsEnabledForName[name] = all
+									self.angleGraphsEnabledForName[name2] = nil
+									self.changedCheckbox = true
+								end
 							end
 						end
 					end
 				end
-			end
-			for i,ni in ipairs(planetNames) do
-				if i ~= j then
-					ig.igPushIDStr(''..i)
-					for k,nk in ipairs(planetNames) do
-						if k ~= j then
-							ig.igPushIDStr(''..k)
-							local name = table{ni,nj,nk}:concat' -> '
-							local name2 = table{nk,nj,ni}:concat' -> '
-							bool[0] = not not (self.graphsEnabledForName[name] or self.graphsEnabledForName[name2])
-							if ig.igCheckbox('', bool) then
-								print('toggling '..name..' to '..bool[0])
-								self.graphsEnabledForName[name] = bool[0] or nil
-								self.changedCheckbox = true
+				for i,ni in ipairs(planetNames) do
+					if i ~= j then
+						ig.igPushIDStr(''..i)
+						for k,nk in ipairs(planetNames) do
+							if k ~= j then
+								ig.igPushIDStr(''..k)
+								local name = table{ni,nj,nk}:concat' -> '
+								local name2 = table{nk,nj,ni}:concat' -> '
+								bool[0] = not not (self.angleGraphsEnabledForName[name] or self.angleGraphsEnabledForName[name2])
+								if ig.igCheckbox('', bool) then
+									self.angleGraphsEnabledForName[name] = bool[0] or nil
+									self.angleGraphsEnabledForName[name2] = bool[0] or nil
+									self.changedCheckbox = true
+								end
+								if ig.igIsItemHovered(ig.ImGuiHoveredFlags_Default) then
+									ig.igBeginTooltip()
+									ig.igText(name)
+									ig.igEndTooltip()
+								end
+								if k ~= #planetNames then
+									ig.igSameLine()
+								end
+								ig.igPopID()
 							end
-							if ig.igIsItemHovered(ig.ImGuiHoveredFlags_Default) then
-								ig.igBeginTooltip()
-								ig.igText(name)
-								ig.igEndTooltip()
-							end
-							if k ~= #planetNames then
-								ig.igSameLine()
-							end
-							ig.igPopID()
 						end
+						ig.igPopID()
 					end
-					ig.igPopID()
 				end
 			end
+			ig.igPopID()
 		end
 		ig.igPopID()
 	end
-	ig.igPopID()
-
+	if ig.igCollapsingHeader'distances:' then
+		ig.igPushIDStr'distances'
+		
+		local all = true	
+		for i=1,#planetNames-1 do
+			if i ~= j then
+				for k=i+1,#planetNames do
+					if k ~= j 
+					and i ~= k 
+					then
+						local ni = planetNames[i]
+						local nk = planetNames[k]
+						local name = table{ni,nk}:concat' <-> '
+						local name2 = table{nk,ni}:concat' <-> '
+						if not self.distanceGraphsEnabledForName[name] 
+						and not self.distanceGraphsEnabledForName[name2] 
+						then
+							all = false
+							break
+						end
+					end
+					if not all then break end
+				end
+				if not all then break end
+			end
+		end
+		bool[0] = all
+		if ig.igCheckbox('all', bool) then
+			all = bool[0]
+			for i=1,#planetNames-1 do
+				if i ~= j then
+					for k=i+1,#planetNames do
+						if k ~= j 
+						and i ~= k
+						then
+							local ni = planetNames[i]
+							local nk = planetNames[k]
+							local name = table{ni,nk}:concat' <-> '
+							local name2 = table{nk,ni}:concat' <-> '
+							self.distanceGraphsEnabledForName[name] = all
+							self.distanceGraphsEnabledForName[name2] = nil
+							self.changedCheckbox = true
+						end
+					end
+				end
+			end
+		end
+		
+		for i,ni in ipairs(planetNames) do
+			ig.igPushIDStr(''..i)
+			for k,nk in ipairs(planetNames) do
+				ig.igPushIDStr(''..k)
+				local name = table{ni,nk}:concat' <-> '
+				local name2 = table{nk,ni}:concat' <-> '
+				bool[0] = not not (self.distanceGraphsEnabledForName[name] or self.distanceGraphsEnabledForName[name2])
+				if ig.igCheckbox('', bool) then
+					self.distanceGraphsEnabledForName[name] = bool[0] or nil
+					self.distanceGraphsEnabledForName[name2] = bool[0] or nil
+					self.changedCheckbox = true
+				end
+				if ig.igIsItemHovered(ig.ImGuiHoveredFlags_Default) then
+					ig.igBeginTooltip()
+					ig.igText(name)
+					ig.igEndTooltip()
+				end
+				if k ~= #planetNames then
+					ig.igSameLine()
+				end
+				ig.igPopID()
+			end
+			ig.igPopID()
+		end
+		ig.igPopID()
+	end
 	App.super.updateGUI(self)
 end
 
